688221131ca72cdc426b43617538ccdae2b2b404
guiosouza <guilhermedeoliveiradesouza@gmail.com>
2022-06-29 19:55:36 -0300

    Product memory

--- src/main/java/com/guiosouza/projetowebbasico/ProjetowebbasicoApplication.java
+++ src/main/java/com/guiosouza/projetowebbasico/ProjetowebbasicoApplication.java
@@ -1,18 +1,25 @@
 package com.guiosouza.projetowebbasico;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.guiosouza.projetowebbasico.entities.Category;
+import com.guiosouza.projetowebbasico.entities.Product;
 import com.guiosouza.projetowebbasico.repositories.CategoryRepository;
+import com.guiosouza.projetowebbasico.repositories.ProductRepository;
 
 @SpringBootApplication
 public class ProjetowebbasicoApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetowebbasicoApplication.class, args);
@@ -22,9 +29,23 @@ public class ProjetowebbasicoApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Category cat1 = new Category(1L, "Electronics");
 		Category cat2 = new Category(2L, "Books");
+
+		Product p1 = new Product(1L, "TV", 2200.00, cat1);
+		Product p2 = new Product(2L, "Domain Driven Design", 120.00, cat2);
+		Product p3 = new Product(3L, "PS5", 2800.00, cat1);
+		Product p4 = new Product(4L, "Docker", 100.00, cat2);
+
+		cat1.getProducts().addAll(Arrays.asList(p1, p3));
+		cat2.getProducts().addAll(Arrays.asList(p2, p4));
 		
 		categoryRepository.save(cat1);
 		categoryRepository.save(cat2);
+		
+		productRepository.save(p1);
+		productRepository.save(p2);
+		productRepository.save(p3);
+		productRepository.save(p4);
+
 	}
 
 }
--- src/main/java/com/guiosouza/projetowebbasico/entities/Product.java
+++ src/main/java/com/guiosouza/projetowebbasico/entities/Product.java
@@ -1,29 +1,29 @@
 package com.guiosouza.projetowebbasico.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-public class Category implements Serializable {
-	
+public class Product implements Serializable {
+	/**
+	 * 
+	 */
 	private static final long serialVersionUID = 1L;
 	private Long id;
 	private String name;
+	private Double price; // double wappler
 	
-	@JsonIgnore
-	private List<Product> products = new ArrayList<>(); 
+	private Category category;
 	
-	public Category () {
+	public Product() {
 		
 	}
 
-	public Category(Long id, String name) {
+	public Product(Long id, String name, Double price, Category category) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.price = price;
+		this.category = category;
 	}
 
 	public Long getId() {
@@ -41,13 +41,22 @@ public class Category implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
 
-	public List<Product> getProducts() {
-		return products;
+	public Double getPrice() {
+		return price;
 	}
-	
-	// HashCode Equals é para um critério personalizado de comparação
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
 
 	@Override
 	public int hashCode() {
@@ -62,9 +71,7 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
 }
